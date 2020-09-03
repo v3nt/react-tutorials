6ecc8103eca9c7d213215fd9fabe6f5a4a84dbcd
v3nt <dan@jynk.net>
2020-09-03 10:25:53 +0100

    optional refactor

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,21 +4,52 @@ import jsonPlaceholder from "../apis/jsonPlaceholder.js";
 // major refactoring of export and function within
 // https://www.udemy.com/course/react-redux/learn/lecture/12586868#overview
 
+// action crator to call both. Alt method to reduce get requests
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // when calling aciton crator inside another action creator we need to call dispatch
+  console.log("fetchPostsAndUsers called");
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  console.log(userIds);
+  console.log("fetchPostsAndUsers complete");
+
+  // optional refactor, chain need value at the end.
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+//  alternat method to reduce user get requests
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// refactor memoized
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
 
 // example memoize : only allows to be called if a new or unique arg
+// ont work if user data has been updated since app started
 
 // export const fetchUser = function (id) {
 //   return _.memoize(async function (dispatch) {
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
+// import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
   componentDidMount() {
-    this.props.fetchUser(this.props.userId);
+    // rm for us to use fetchPostsAndUsers
+    // this.props.fetchUser(this.props.userId);
   }
   render() {
     // console.log(this.props.userId);
@@ -32,7 +33,7 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
 
 // testing on loDash.com
 
