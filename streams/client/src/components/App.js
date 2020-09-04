929ece24b8aae1e158734ef2aff05655f2b59a48
v3nt <dan@jynk.net>
2020-09-04 11:36:53 +0100

    BrowserRouter

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>page one</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two <button>Click</button>
+    </div>
+  );
+};
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" exact component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
