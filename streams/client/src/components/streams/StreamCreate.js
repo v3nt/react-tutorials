42d4dbfe386c57dd734a3b1df745eb0fd76452d4
v3nt <dan@jynk.net>
2020-09-17 11:00:42 +0100

    adding streams to json

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,6 +1,8 @@
 import React from "react";
 // componenents should have Sentence case words and function camelback format.
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 // convert to class so you can use helper methonds
 // const StreamCreate = () => {
@@ -32,11 +34,13 @@ class StreamCreate extends React.Component {
       </div>
     );
   };
-
-  onSubmit(formValues) {
+  // TypeError: Cannot read property 'props' of undefined
+  // fixed by using arrow function format
+  onSubmit = (formValues) => {
     // console.log(formValues);
     // event.preventDefault(); // not needed with redux-form
-  }
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -72,9 +76,29 @@ const validate = (formValues) => {
 };
 
 // returns a function that is immediately calle with (StreamCreate)
-export default reduxForm({
+
+// how to wire up redux form and connect ?
+// export default reduxForm({
+//   form: "StreamCreate",
+//   validate,
+// })(StreamCreate);
+
+// solution 1
+
+// export default connect()(
+//   eduxForm({
+//     form: "StreamCreate",
+//     validate,
+//   })(StreamCreate)
+// );
+
+// option 2
+
+const formWrapped = reduxForm({
   form: "StreamCreate",
   validate,
 })(StreamCreate);
 
-// if samre reduce validate:validate to one calidate
+export default connect(null, { createStream })(formWrapped);
+
+// if same reduce validate:validate to one calidate
