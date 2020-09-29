b188abf8b267bd0d5cd8d3a1c6df4c12f822ea11
v3nt <dan@jynk.net>
2020-09-29 09:32:30 +0100

    language state

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+    console.log(language);
+  };
+
   render() {
-    return <div>APP</div>;
+    return (
+      <div className="ui container">
+        <h1>APP</h1>
+        <div>
+          Select language:
+          <i
+            className="flag uk"
+            onClick={() => this.onLanguageChange("English UK")}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("Dutch")}
+          ></i>
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
