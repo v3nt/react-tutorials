94d3661f04adebf98779c6484966a10bb3402c8f
v3nt <dan@jynk.net>
2020-09-30 17:32:17 +0100

    wiki list loaded html printed

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
+  const [term, setTerm] = useState("soil needs worms");
+  const [results, setResults] = useState([]);
+  console.log(results);
+  useEffect(() => {
+    // can't mark function with async or await in >useEffect ;-()
+    // #1 const search() ...
+    // so we can call a function that doesn't have a 'name'
+    // #2
+    //
+    if (term) {
+      (async () => {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: term,
+          },
+        });
+        setResults(data.query.search);
+      })();
+    }
+    // #3
+    // axios.get("abc").then((response) => {});
+    //
+  }, [term]);
+  // array can have multiple items too.  const [termb, setTermb] ...
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key="{result.pageid} ">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <div className="snippet">{result.snippet}</div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Search terms</label>
-          <input className="input" />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
+      <ul className="ui big divided list">{renderedResults}</ul>
     </div>
   );
 };
