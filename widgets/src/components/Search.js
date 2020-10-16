1c344b426e29616519cbfdb9c32ed167226714b2
v3nt <dan@jynk.net>
2020-10-16 11:33:49 +0100

    deboinced complete

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -4,11 +4,20 @@ import DOMPurify from "dompurify";
 
 const Search = () => {
   const [term, setTerm] = useState("soil needs worms");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log("init & term changed");
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
@@ -16,26 +25,16 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
+    if (term) {
       search();
     } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
-      return () => {
-        console.log("cleanup " + timeoutId);
-        clearTimeout(timeoutId);
-      };
     }
-  }, [term, results.length]); // dependency array.
+  }, [debouncedTerm]);
 
   // array can have multiple items too.  const [termb, setTerm] ...
 
