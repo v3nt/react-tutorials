441b9eddfb448569cbdef9153cbf1248c899d769
v3nt <dan@jynk.net>
2020-10-20 10:06:33 +0100

    active link attempt

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Link = ({ className, href, children }) => {
+  const [active, setActive] = useState("");
+
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     window.history.pushState({}, "", href);
     const navEvent = new PopStateEvent("popstate");
     window.dispatchEvent(navEvent);
+    setActive("active");
   };
 
   return (
-    <a onClick={onClick} className={className} href={href}>
+    <a onClick={onClick} className={`item ${active}`} href={href}>
       {children}
     </a>
   );
